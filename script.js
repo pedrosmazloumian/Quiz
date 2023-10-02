858d8e1f5cd599d68db9e0d35893ff663f611643
pedrosmazloumian <98281733+pedrosmazloumian@users.noreply.github.com>
2023-10-02 12:02:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ function sendMail() {
       name: document.getElementById("name").value,
       email: document.getElementById("email").value,
       Branch: document.getElementById("Branch").value,
-      //message: document.getElementById("message").value,
+      name_1: document.getElementById("name_1").value,
+      Branch_1: document.getElementById("Branch_1").value,
+      email_1: document.getElementById("email_1").value,
+      name_2: document.getElementById("name_2").value,
+      Branch_2: document.getElementById("Branch_2").value,
+      email_2: document.getElementById("email_2").value,
+      name_3: document.getElementById("name_3").value,
+      Branch_3: document.getElementById("Branch_3").value,
+      email_3: document.getElementById("email_3").value,
+      // message: document.getElementById("message").value,
 
     };
   
@@ -17,7 +26,16 @@ function sendMail() {
           document.getElementById("name").value = "";
           document.getElementById("email").value = "";
           document.getElementById("Branch").value = "";
-          //document.getElementById("message").value = "";
+          document.getElementById("name_1").value= "";
+          document.getElementById("Branch_1").value= "";
+          document.getElementById("email_1").value= "";
+          document.getElementById("name_2").value= "";
+          document.getElementById("Branch_2").value= "";
+          document.getElementById("email_2").value= "";
+          document.getElementById("name_3").value= "";
+          document.getElementById("Branch_3").value= "";
+          document.getElementById("email_3").value= "";
+          // document.getElementById("message").value = "";
           console.log(res);
           alert("Your message sent successfully!!")
   
